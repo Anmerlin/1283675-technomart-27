9838790bbdc95ef807de35eaef7b8e0daa73a11c
Mirddin Emris <anmerlin@mail.ru>
2020-03-09 21:17:25 +0300

    Правит стили каталога

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
+var slides = document.querySelectorAll(".tizers-slide");
 var slideBtnNext = document.querySelector(".tizers-slider-wrapper .arrow-next");
 var slideBtnPrev = document.querySelector(".tizers-slider-wrapper .arrow-prev");
 var slideDot = document.querySelectorAll(".slider-pagination .slider-dot");
 
 var slideIndex = 1;
-showSlides(slideIndex);
+if (slides.length) {
+  showSlides(slideIndex);
 
-slideBtnNext.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  nextSlide()
-});
-
-slideBtnPrev.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  prevSlide()
-});
-
-slideDot.forEach(function (dot) {
-  dot.addEventListener("click", function (evt) {
+  slideBtnNext.addEventListener("click", function (evt) {
     evt.preventDefault();
-    currentSlide(+dot.innerHTML);
+    nextSlide()
   });
-});
+
+  slideBtnPrev.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    prevSlide()
+  });
+
+  slideDot.forEach(function (dot) {
+    dot.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      currentSlide(+dot.innerHTML);
+    });
+  });
+}
 
 function nextSlide() {
   showSlides(slideIndex += 1);
@@ -36,7 +39,6 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.querySelectorAll(".tizers-slide");
   var dots = document.querySelectorAll(".slider-pagination .slider-dot");
 
   if (n > slides.length) {
@@ -86,10 +88,12 @@ var feedback = document.querySelector(".modal-feedback");
 var basket = document.querySelector(".modal-basket");
 var close = document.querySelectorAll(".modal-close");
 
-var form = feedback.querySelector("form");
-var surname = feedback.querySelector("[name=surname]");
-var email = feedback.querySelector("[name=email]");
-var text = feedback.querySelector("[name=text]");
+if (feedback !== null && feedback.length) {
+  var form = feedback.querySelector("form");
+  var surname = feedback.querySelector("[name=surname]");
+  var email = feedback.querySelector("[name=email]");
+  var text = feedback.querySelector("[name=text]");
+}
 
 var isStorageSupport = true;
 var storageSurname = "";
@@ -101,24 +105,27 @@ try {
 } catch (err) {
   isStorageSupport = false;
 }
+if (mapLink !== null && mapLink.length) {
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    map.classList.add("modal-show");
+  });
+}
 
-mapLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  map.classList.add("modal-show");
-});
+if (feedbackBtn !== null && feedbackBtn.length) {
+  feedbackBtn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedback.classList.add("modal-show");
 
-feedbackBtn.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  feedback.classList.add("modal-show");
-
-  if (storageSurname && storageEmail) {
-    surname.value = storageSurname;
-    email.value = storageEmail;
-    text.focus();
-  } else {
-    surname.focus();
-  }
-});
+    if (storageSurname && storageEmail) {
+      surname.value = storageSurname;
+      email.value = storageEmail;
+      text.focus();
+    } else {
+      surname.focus();
+    }
+  });
+}
 
 basketBtn.forEach(function (item) {
   item.addEventListener("click", function (evt) {
@@ -134,14 +141,16 @@ close.forEach(function (item) {
   });
 });
 
-form.addEventListener("submit", function (evt) {
-  if (!surname.value || !email.value) {
-    evt.preventDefault();
-    // error
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("surname", surname.value);
-      localStorage.setItem("email", email.value);
+if (form !== undefined && form.length) {
+  form.addEventListener("submit", function (evt) {
+    if (!surname.value || !email.value) {
+      evt.preventDefault();
+      // error
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("surname", surname.value);
+        localStorage.setItem("email", email.value);
+      }
     }
-  }
-});
+  });
+}
